81f2ce758f717cd7f02629e6e230250403e94f63
prastowoardi <prastowoardi616@gmail.com>
2023-01-14 06:29:52 +0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ for(let i=0; i<=100; i++){
         ganjil = [...ganjil, i]
     }
 }
-
 console.log({ganjil, genap})
 
 
@@ -47,51 +46,52 @@ for(let i=0; i<=15; i++){
 console.log({fibonancy})
 
 // soal 4
-let uangAndi = 750000;
-let hasil = "";
+let uangAndi = 750000
+let hasil = ""
 for(let i=1; i<=12; i++){
-    let bunga = 0;
-    let sisih = 0;
+    let bunga = 0
+    let sisih = 0
     if(i>1){
-        bunga = uangAndi*6/100;
+        bunga = uangAndi*6/100
         if(uangAndi+bunga > 800000){
-            sisih = 800000*3/100;
+            sisih = 800000*3/100
         }else{
-            sisih = (uangAndi+bunga)*3/100;
+            sisih = (uangAndi+bunga)*3/100
         }
     }
 
-    hasil += `Tabungan Awal : ${uangAndi.toFixed(2)} \n Bunga : ${bunga.toFixed(2)} \n Sisih : ${sisih.toFixed(2)} \n Uang Bulang ${i} : ${((uangAndi+bunga)-sisih).toFixed(2)} \n------------------\n`;
-    uangAndi = (uangAndi+bunga)-sisih;
+    hasil += `Tabungan Awal : ${uangAndi.toFixed(2)},  Bunga : ${bunga.toFixed(2)},  Sisih : ${sisih.toFixed(2)}, Uang Bulang ${i} : ${((uangAndi+bunga)-sisih).toFixed(2)} `
+    uangAndi = (uangAndi+bunga)-sisih
 }
-console.log({hasil});
+
+console.log({hasil})
 
 // Soal No 5
 
-const dataArray         = [1,4,5,3,2,5,3,4,8,5,4,3,6,0,4,3,2,1,5,9,9,4,3,1,3,0,2,2,4,6,2,4,7,4];
-let dataArrayTampung    = [];
-let dataArrayCount      = "";
+const dataArray         = [1,4,5,3,2,5,3,4,8,5,4,3,6,0,4,3,2,1,5,9,9,4,3,1,3,0,2,2,4,6,2,4,7,4]
+let dataArrayTampung    = []
+let dataArrayCount      = ""
 
 for(let i=0; i<dataArray.length; i++){
-    let ketemu = false;
+    let ketemu = false
 
     for(let a=0; a<dataArrayTampung.length; a++){
         if(dataArray[i] == dataArrayTampung[a]){
-            ketemu = true;
+            ketemu = true
         }
     }
 
     if(!ketemu){
-        dataArrayTampung = [...dataArrayTampung, dataArray[i]];
+        dataArrayTampung = [...dataArrayTampung, dataArray[i]]
 
-        let totalKetemu = 0;
+        let totalKetemu = 0
         for(let e=0; e<dataArray.length; e++){
             if(dataArray[i] == dataArray[e]){
-                totalKetemu = totalKetemu+1;
+                totalKetemu = totalKetemu+1
             }
         }
 
-        dataArrayCount += `Angka ${dataArray[i]} sebanyak ${totalKetemu} \n`;
+        dataArrayCount += `Angka ${dataArray[i]} sebanyak ${totalKetemu}, `
     }
 }
 
